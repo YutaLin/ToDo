cea79963d8ecf37d523a926fe11cf0e4b6b8c958
linyuta <linyuta@linyuta.local>
2017-11-30 11:41:51 +0800

    new init func for default nil description

--- ToDo/ToDoItem.swift
+++ ToDo/ToDoItem.swift
@@ -11,4 +11,9 @@ import Foundation
 struct ToDoItem {
     let title: String
     let itemDescription: String?
+    
+    init(title: String, itemDescription: String? = nil) {
+        self.title = title
+        self.itemDescription = itemDescription
+    }
 }
