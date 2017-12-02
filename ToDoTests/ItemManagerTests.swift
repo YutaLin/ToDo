7a3e1b29db3a16dee4742b84621b43ea202d3439
linyuta <linyuta@linyuta.local>
2017-12-02 16:08:16 +0800

    add new additem, removeitem function for check item in list in ItemManager new testing function for check item in list

--- ToDoTests/ItemManagerTests.swift
+++ ToDoTests/ItemManagerTests.swift
@@ -33,4 +33,34 @@ class ItemManagerTests: XCTestCase {
         XCTAssertEqual(sut.doneCount, 0)
     }
     
+    func test_AddItem_IncreasesToDoCountToOne() {
+        sut.add(ToDoItem(title: ""))
+        
+        XCTAssertEqual(sut.toDoCount, 1)
+    }
+    
+    func test_ItemAt_ReturnsAddedItem() {
+        let item = ToDoItem(title: "Foo")
+        sut.add(item)
+        
+        let returnedItem = sut.item(at: 0)
+        XCTAssertEqual(returnedItem.title, item.title)
+    }
+    
+    func test_CheckItemAt_ChangesCounts() {
+        sut.add(ToDoItem(title: ""))
+        sut.checkItem(at: 0)
+        
+        XCTAssertEqual(sut.toDoCount, 0)
+        XCTAssertEqual(sut.doneCount, 1)
+    }
+    
+    func test_DoneItemAt_ReturnsCheckedItem() {
+        let item = ToDoItem(title: "Foo")
+        sut.add(item)
+        sut.checkItem(at: 0)
+        let returnItem = sut.doneItem(at: 0)
+        
+        XCTAssertEqual(returnItem.title, item.title)
+    }
 }
