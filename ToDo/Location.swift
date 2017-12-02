9dd94c036ece144d2b59190e986f4f051c8fab50
linyuta <linyuta@linyuta.local>
2017-12-02 17:12:09 +0800

    make struct Location, ToDoItem conform protocol Equatable and test it

--- ToDo/Location.swift
+++ ToDo/Location.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CoreLocation
 
-struct Location {
+struct Location: Equatable {
     let name: String
     let coordinate: CLLocationCoordinate2D?
     
@@ -18,3 +18,21 @@ struct Location {
         self.coordinate = coordinate
     }
 }
+
+func ==(lhs: Location, rhs: Location) -> Bool {
+    if lhs.coordinate?.latitude != rhs.coordinate?.longitude {
+        return false
+    }
+
+    if lhs.coordinate?.longitude != rhs.coordinate?.longitude {
+        return false
+    }
+    
+    if lhs.name != rhs.name {
+        return false
+    }
+    
+    return true
+}
+
+
